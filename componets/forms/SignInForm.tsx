29c0d396f15c15c28728be9319ffbae5f7fb9764
jerobe <jerobejrobinson@gmail.com>
2023-01-07 07:48:01 -0600

    using material ui

--- componets/forms/SignInForm.tsx
+++ componets/forms/SignInForm.tsx
@@ -1,6 +1,7 @@
 import { signInWithEmail } from "@/utils/auth";
 import { useEffect, useState } from "react";
 import { useForm, SubmitHandler } from 'react-hook-form';
+import Button from '@mui/material/Button'
 
 type Inputs = {
     email: string,
@@ -8,7 +9,19 @@ type Inputs = {
 }
 export default function SignInForm() {
     const { register, handleSubmit, reset, formState: { errors, isSubmitSuccessful }} = useForm<Inputs>();
-    const onSubmit: SubmitHandler<Inputs> = data => signInWithEmail(data.email, data.password);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const onSubmit: SubmitHandler<Inputs> = async data => {
+        setIsLoading(true);
+        try {
+            await signInWithEmail(data.email, data.password)
+        } catch(error) {
+            let message = 'unkown error';
+            if(error instanceof Error) message = error.message
+            setErrorMessage(message);
+        }
+    };
 
     useEffect(() => {
         reset()
@@ -16,9 +29,12 @@ export default function SignInForm() {
    
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input {...register("email", { required: true })} placeholder=" Email" type="email"/>
+            <input 
+                placeholder=" Email" 
+                {...register("email", {required: true, pattern: /^\S+@\S+$/i })} />
             <input {...register("email", { required: true })} placeholder=" Password" type="password"/>
             <button type="submit">Sign In</button>
+            <Button >Sign In</Button>
         </form>
     )
 }
